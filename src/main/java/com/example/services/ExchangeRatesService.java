45731342ed50b8f93f4cd4600fa1de4dfa62c719
Nataly Tereshkova <86656038+Nataly-git@users.noreply.github.com>
2022-05-31 21:51:26 +0300

    add gap calculation

--- src/main/java/com/example/services/ExchangeRatesService.java
+++ src/main/java/com/example/services/ExchangeRatesService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ExchangeRatesService {
 
-    public List<ExchangeRatesDTO> getExchangeRates();
+    public Double getExchangeRatesGap();
 
 
 }
--- src/main/java/com/example/services/ExchangeRatesServiceImpl.java
+++ src/main/java/com/example/services/ExchangeRatesServiceImpl.java
@@ -2,8 +2,11 @@ package com.example.services;
 
 import com.example.clients.ExchangeRatesFeignClient;
 import com.example.dto.ExchangeRatesDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -15,9 +18,23 @@ public class ExchangeRatesServiceImpl  implements ExchangeRatesService{
         this.client = client;
     }
 
-    @Override
-    public List<ExchangeRatesDTO> getExchangeRates() {
+    @Value("${exchangerates.app_id}")
+    private String appId;
 
-        return null;
+    @Value("${exchangerates.currency}")
+    private String currency;
+
+
+    @Override
+    public Double getExchangeRatesGap() {
+        Double currentRate = client.getLatestExchangeRates(appId).getRates().get(currency);
+        Double recentRate = client.getHistoricalExchangeRates(getYesterdayDate(), appId).getRates().get(currentRate);
+        return currentRate - recentRate;
+    }
+
+    private String getYesterdayDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+        return formatter.format(yesterday);
     }
 }
